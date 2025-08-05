eca9a28cf0c818f8c80e95a05c4d77b7ed3907a8
AndoAAA <tarverdyan070@gmail.com>
2025-08-05 17:52:23 +0400

    build product card & card carousel

---
+++ src/components/CardCarousel.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import Link from "next/link";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import ProductCard from "./ProductCard";
+
+interface Card {
+  image: string;
+  text: string;
+  price: string;
+}
+
+interface CardCarouselProps {
+  title: string;
+  cards: Card[];
+}
+
+const CardCarousel: React.FC<CardCarouselProps> = ({ title, cards }) => {
+  return (
+    <div className="w-full max-w-7xl mx-auto my-12 px-4">
+      <div className="flex justify-between items-center mb-6">
+        <h2 className="text-xl sm:text-3xl md:text-4xl font-bold text-gray-800">
+          {title}
+        </h2>
+        <Link href="/products">
+          <span className="text-lg text-gray-600 hover:text-[#a01f64]">
+            View More
+          </span>
+        </Link>
+      </div>
+
+      <div className="relative">
+        <Swiper
+          modules={[Navigation, Pagination]}
+          spaceBetween={20}
+          slidesPerView={1}
+          breakpoints={{
+            640: { slidesPerView: 2 },
+            768: { slidesPerView: 3 },
+            1024: { slidesPerView: 4 },
+          }}
+          navigation
+          pagination={{ clickable: true }}
+          className="card-swiper"
+        >
+          {cards.map((card, index) => (
+            <SwiperSlide key={index} className="mb-10">
+              <ProductCard
+                image={card.image}
+                text={card.text}
+                price={card.price}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default CardCarousel;
--- src/home/page.tsx
+++ src/home/page.tsx
@@ -1,12 +1,16 @@
 import Categories from "@/components/Categories";
+import NewArrivals from "@/components/NewArrivals";
 import Slider from "@/components/Slider";
+import TopSellers from "@/components/TopSellers";
 import React from "react";
 
 const HomePage = () => {
   return (
     <div>
       <Slider />
-      <Categories/>
+      <Categories />
+      <NewArrivals />
+      <TopSellers />
     </div>
   );
 };

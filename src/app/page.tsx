ed7051835ae60f5baba3886bf03a7466b806a911
AndoAAA <tarverdyan070@gmail.com>
2025-09-10 22:46:10 +0400

    building a new movies app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="min-h-screen bg-blue-100 flex items-center justify-center">
-        <h1 className="text-4xl font-bold text-blue-900">
-          Բարև Tailwind + Next.js
-        </h1>
-      </div>
+      <div className="bg-black min-h-screen text-white"></div>
     </>
   );
 }

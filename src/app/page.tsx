bbe4d7b79c4f43e779347123848731ee9ffb2815
AndoAAA <tarverdyan070@gmail.com>
2025-09-17 00:08:46 +0400

    creating hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
     <>
-      <div className="bg-black min-h-screen text-white"></div>
+      <div className="bg-black min-h-screen text-white">
+        <HeroSection />
+      </div>
     </>
   );
 }
